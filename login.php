085b318133528231c6e261aae9ed57d12f6ea4bc
1300427 <1300427@rgu.ac.uk>
2016-03-02 16:31:00 +0000

    update css 6

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if(empty($_POST ["username"])|| empty($_POST ["password"]))
     $password=$_POST ["password"];
     $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
     $result=mysqli_query($db,$sql);
-    if(mysqli_num_rows($result)== 1 )
+    if(mysqli_num_rows($result)== 1)
     {
         header ("location: home.php"); // Redirecting to another page
     }else
