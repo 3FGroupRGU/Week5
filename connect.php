1a9a05ef7bbbc134e316db6c3c622690d931481b
1300427 <1300427@rgu.ac.uk>
2016-03-02 15:21:12 +0000

    Initial commit

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b679b44245bd');
+define('DB_PASSWORD', 'c0bf782c');
+define('DB_DATABASE', 'CMM503_1300427');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
