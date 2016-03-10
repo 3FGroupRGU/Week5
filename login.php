5b601bfbbf54b5c261d98e604fde6c1552b1cb4d
HenryRL <1300427@rgu.ac.uk>
2016-03-10 15:22:31 +0000

    Added files via upload

--- login.php
+++ login.php
@@ -7,12 +7,26 @@ if (empty($_POST ["username"]) || empty($_POST ["password"])) {
     $password = $_POST ["password"];
     echo $username;
     echo $password;
-    $sql = "SELECT * FROM users WHERE username='$username' and password='$password'";
-    $result = mysqli_query($db, $sql);
+    $MySDql = "SELECT * FROM users WHERE username='$username' and password='$password'";
+    $result = mysqli_query($db, $MySql);
     if (mysqli_num_rows($result) == 1) {
         header("location: home.php"); // Redirecting to another page
     } else {
         echo "Incorrect username or password";
     }
 }
+    setcookie(‘access_level’,'standarduser');
+
+    $_SESSION['foo'] = 'bar';
+        print $_SESSION['foo'];
+        unset($_SESSION['foo']);
+?>
+
+<?php
+    session_start ();
+    if (isset ($_session ['FirstName'])) {
+        ///your code here
+    }
+    $_session = array ();
+    session_destroy ();
 ?>
